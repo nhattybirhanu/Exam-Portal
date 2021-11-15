64bc095f7dd2461c02a27ee0d061076e91b80493
adamkahsai <84365217+adamkahsai@users.noreply.github.com>
2021-11-14 19:16:02 -0600

    First COmmit

---
+++ proffesior-exam-creator/proffesior-exam-creator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proffesior-exam-creator',
+  templateUrl: './proffesior-exam-creator.component.html',
+  styleUrls: ['./proffesior-exam-creator.component.css']
+})
+export class ProffesiorExamCreatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
