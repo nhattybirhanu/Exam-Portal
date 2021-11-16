fbe3a3d80c8248c8f25c75f1ccb7c0c690726d6e
adamkahsai <84365217+adamkahsai@users.noreply.github.com>
2021-11-15 23:00:25 -0600

    update

---
+++ src/app/prof-student/prof-student.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prof-student',
+  templateUrl: './prof-student.component.html',
+  styleUrls: ['./prof-student.component.css']
+})
+export class ProfStudentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
